fbfbb8f3339e91efc1ae9cfce1eefebd22c2afcd
kenjick <kenjick@kenjicks-MacBook-Pro.local>
2026-04-20 09:26:28 +0900

    Exclude qh129 and qh138 from PBS jobs — both have uncorrectable ECC errors

--- jobs/bench_4node.sh
+++ jobs/bench_4node.sh
@@ -4,7 +4,7 @@
 #              from /groups/input/internvideo25/internvideo_2_5/
 #PBS -N bench_4node
 #PBS -q rt_HF
-#PBS -l select=4
+#PBS -l select=4:host!=qh138:host!=qh129
 #PBS -l walltime=02:00:00
 #PBS -j oe
 #PBS -o logs/bench_4node/
